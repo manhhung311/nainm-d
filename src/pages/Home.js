d61e77f57492ec7956ad78233916ed469ba11fc5
Le Dat <datcatt1123@gmail.com>
2024-04-03 14:01:17 +0700

    update home screen content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,6 +14,7 @@ export default function HomePage() {
     <Page title="Trang chủ">
       <RootStyle>
 
+
         <HomeHero/>
       </RootStyle>
     </Page>
--- src/sections/home/HomeHero.js
+++ src/sections/home/HomeHero.js
@@ -8,31 +8,35 @@ import { useTheme } from '@mui/material/styles';
 import React from 'react';
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
-import Image from '../../components/Image';
+import { Link } from 'react-router-dom';
 import useResponsive from '../../hooks/useResponsive';
+import Image from '../../components/Image';
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const images = [
   {
     label: '1',
-    title:
-      'We focus on the development of soft actuators and artificial muscle, which can be applied to intra/extra human body robotics, haptic-feedback systems, wearable power suits, and flexible and soft electronics.',
+    title: 'Artificial Muscle and Soft Robotics',
+    Content: 'We focus on the development of soft actuators and artificial muscle, which can be applied to intra/extra human body robotics, haptic-feedback systems, wearable power suits, and flexible and soft electronics.',
     imgPath: 'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
   },
   {
     label: '2',
-    title: 'ảnh 2',
+    title: 'Energy Harvesting and Triboelectricity',
+    Content: 'We design energy harvesting systems and triboelectric nanogenerators (TENGs),which can produce electricity from mechanical kinetic energy.',
     imgPath: 'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
   },
   {
     label: '3',
-    title: 'ảnh 3',
+    title: ' Energy Storage and Structural Battery',
+    Content: 'We develop structural battery possessing both load bearing capability and energy storage function.The structural batteries are crucial to the next-generation transportation vehicles such as UAV, UAM, and electrical vehicles.',
     imgPath: 'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
   },
   {
     label: '4',
-    title: 'ảnh 4',
+    title: 'Mechanical Metamaterial and Smart Structures',
+    Content: 'We investigate mechanical metamaterials and topological insulators,which show the unparalleled wave transmission and acoustic bandgap.',
     imgPath:
       'https://ghepanhpro.com/uploads/w450/2021/09/02/tao-khung-anh-xe-giay-online61306a9ad4e9d_f6f9f92616193460e8380b0a8bb630f7.jpg',
   },
@@ -108,9 +112,9 @@ export default function HomeHero() {
                       sx={{ flexDirection: { xs: 'column-reverse', sm: 'row', md: 'row' } }}
                     >
                       <Grid item xs={12} sm={6} md={4}>
-                        <Image style={{ height: '400px', width: '100%' }} src={step.imgPath} alt={step.label} />
+                        <img style={{ height: '400px', width: '100%' }} src={step.imgPath} alt={step.label} />
                       </Grid>
-                      <Grid item xs={12} sm={6} md={8}>
+                      <Grid item xs={12} sm={6} md={8} >
                         <Typography
                           variant="h4"
                           gutterBottom
@@ -122,13 +126,20 @@ export default function HomeHero() {
                             fontSize: '0.1rem',
                             textAlign: 'center',
                             lineHeight: '50px',
+
                           }}
                         >
                           {step.label}
                         </Typography>
-                        <Typography variant="body2" color="textSecondary">
+                        <Typography variant="h3" color="#82f9d4">
                           {step.title}
                         </Typography>
+                        <Typography variant="subtitle1" color='#fff' >
+                          {step.Content}
+                        </Typography>
+                        <Link to="/more-view" sx={{
+                          height: '50px',
+                        }}>More View</Link>
                       </Grid>
                     </Grid>
                   ) : null}
@@ -164,7 +175,7 @@ export default function HomeHero() {
 
             <Grid item xs={12} md={2.2} sx={{ alignSelf: 'center', textAlign: 'center', alignItems: 'center' }}>
               <Box
-                onClick={() => handleItemClick(0)} // Chuyển đến hình ảnh 1 khi click vào mục "Articial Muscle and Soft Robottics"
+                onClick={() => handleItemClick(4)} // Chuyển đến hình ảnh 1 khi click vào mục "Articial Muscle and Soft Robottics"
                 sx={{
                   maxWidth: '210px',
                   color: '#fff',
@@ -180,7 +191,7 @@ export default function HomeHero() {
             </Grid>
             <Grid item xs={12} md={2.2}>
               <Box
-                onClick={() => handleItemClick(1)} // Chuyển đến hình ảnh 2 khi click vào mục "Energy Harvesting and Triboelectricity"
+                onClick={() => handleItemClick(0)} // Chuyển đến hình ảnh 2 khi click vào mục "Energy Harvesting and Triboelectricity"
                 sx={{ maxWidth: '210px', display: 'flex', color: '#fff' }}
               >
                 <Box sx={{ width: '48px', height: '27px', fontSize: '1.5rem' }}>01</Box>
@@ -193,7 +204,7 @@ export default function HomeHero() {
             </Grid>
             <Grid item xs={12} md={2.2}>
               <Box
-                onClick={() => handleItemClick(2)} // Chuyển đến hình ảnh 3 khi click vào mục "Energy Storage and Structural Battery"
+                onClick={() => handleItemClick(1)} // Chuyển đến hình ảnh 3 khi click vào mục "Energy Storage and Structural Battery"
                 sx={{ maxWidth: '210px', display: 'flex', color: '#fff' }}
               >
                 <Box sx={{ width: '48px', height: '27px', fontSize: '1.5rem' }}>02</Box>
@@ -206,7 +217,7 @@ export default function HomeHero() {
             </Grid>
             <Grid item xs={12} md={2.2}>
               <Box
-                onClick={() => handleItemClick(3)} // Chuyển đến hình ảnh 4 khi click vào mục "Mechnical Metamaterial and Smart Structures"
+                onClick={() => handleItemClick(2)} // Chuyển đến hình ảnh 4 khi click vào mục "Mechnical Metamaterial and Smart Structures"
                 sx={{ maxWidth: '210px', display: 'flex', color: '#fff' }}
               >
                 <Box sx={{ width: '48px', height: '27px', fontSize: '1.5rem' }}>03</Box>
@@ -219,7 +230,7 @@ export default function HomeHero() {
             </Grid>
             <Grid item xs={12} md={2.2}>
               <Box
-                onClick={() => handleItemClick(4)} // Chuyển đến hình ảnh 5 khi click vào mục "Mechnical Metamaterial and Smart Structures"
+                onClick={() => handleItemClick(3)} // Chuyển đến hình ảnh 5 khi click vào mục "Mechnical Metamaterial and Smart Structures"
                 sx={{ maxWidth: '210px', display: 'flex', color: '#fff' }}
               >
                 <Box sx={{ width: '48px', height: '27px', fontSize: '1.5rem' }}>04</Box>
