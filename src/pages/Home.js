53ad1865456df8e1d845e02e34029fb6be46e64c
Nguyễn Đức Hải <94112281+HaiK-O@users.noreply.github.com>
2024-04-05 17:01:44 +0700

    05/04/2024

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,14 @@
 // @mui
 import { styled } from '@mui/material/styles';
 // components
-import Page from "../components/Page";
-import HomeHero from "../sections/home/HomeHero";
-import HomeMinimal from "../sections/home/HomeMinimal";
+import Page from '../components/Page';
+import HomeHero from '../sections/home/HomeHero';
+import HomeMinimal from '../sections/home/HomeMinimal';
 // ----------------------------------------------------------------------
 const RootStyle = styled('div')(({ theme }) => ({
   // height: '100%',
   paddingTop: theme.spacing(8),
+
   [theme.breakpoints.up('md')]: {
     paddingTop: theme.spacing(11),
   },
