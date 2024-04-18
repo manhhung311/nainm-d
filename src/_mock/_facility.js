60380cfba650df6e468c1e9af5ecd9332b2baa0f
Nguyễn Hải Dương <duongnotthieunacl2@gmail.com>
2024-04-18 21:13:17 +0700

    facility

---
+++ src/_mock/_facility.js
@@ -0,0 +1,23 @@
+import _mock from './_mock';
+// ----------------------------------------------------------------------
+export const titleFacility = [
+    'Nano Generators and Green Energy',
+    'Goal',
+    'Approach',
+    'Selected Publications',
+    'What is soft robotics?',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    
+  ];
+export const _facilityData = [...Array(titleFacility.length)].map((_, index) => ({
+    image: _mock.image.cover(index),
+  }));
