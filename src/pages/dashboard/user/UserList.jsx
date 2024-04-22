7699b0e4c975f483ec2b31db534b7b01b5b2fcb2
dnhtrien <ntrien50@gmail.com>
2024-04-23 01:43:03 +0700

    tiny fix

--- src/pages/dashboard/user/UserList.jsx
+++ src/pages/dashboard/user/UserList.jsx
@@ -137,8 +137,6 @@ export default function UserList() {
     (!dataFiltered.length && !!filterRole) ||
     (!dataFiltered.length && !!filterStatus);
 
-  console.log('dataFiltered', dataFiltered);
-
   return (
     <Page title={t('user.pageList')}>
       <Container maxWidth={themeStretch ? false : 'lg'}>
