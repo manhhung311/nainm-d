6a0bad08605b8b7250470c09ecd452cf51d46911
Vu Gia Long <longmza13@gmail.com>
2024-05-04 15:02:10 +0700

    fix2

--- src/pages/dashboard/publication/PublictionCreate.jsx
+++ src/pages/dashboard/publication/PublictionCreate.jsx
@@ -1,10 +1,14 @@
 // @mui
-import { styled, alpha } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 import PropTypes from 'prop-types';
 import isString from 'lodash/isString';
 // @mui
 import { LoadingButton } from '@mui/lab';
 import { Box, Button, Container, Typography, DialogActions } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { loader } from 'graphql.macro';
+import { useQuery } from '@apollo/client';
+import { useLocation, useParams } from 'react-router-dom';
 // components
 import Image from '../../../components/Image';
 import PublicationNewForm from '../../../sections/publication/PublicationNewForm';
@@ -12,28 +16,45 @@ import { PATH_DASHBOARD } from '../../../routes/paths';
 import Page from '../../../components/Page';
 import useSettings from '../../../hooks/useSettings';
 import HeaderBreadcrumbs from '../../../components/HeaderBreadcrumbs';
+import useLocales from '../../../locals/useLocals';
+
 
 // ----------------------------------------------------------------------
 const RootStyle = styled('div')(() => ({
   height: '100%',
 }));
 // ----------------------------------------------------------------------
+const DETAIL_COLLECTION = loader('../../../graphql/queries/collections/DetailCollection.graphql');
 
 export default function PublictionCreate() {
+  const { pathname } = useLocation();
+
+  const [postUpdate, setPostUpdate] = useState();
+
+  const { id: idNews } = useParams();
+
+  const { data: detailCollection } = useQuery(DETAIL_COLLECTION, {
+    variables: {
+      id: idNews,
+    },
+  });
+
+  useEffect(() => {
+    if (detailCollection) {
+      setPostUpdate(detailCollection?.collection);
+    }
+  }, [idNews, detailCollection]);
+
+  const isEdit = pathname.includes('edit');
+
+  const { t } = useLocales();
+
   const { themeStretch } = useSettings();
   return (
     <Page title="Blog: New Post">
       <Container maxWidth={themeStretch ? false : 'lg'}>
-        <HeaderBreadcrumbs
-          heading="Create a new post"
-          links={[
-            { name: 'Dashboard', href: PATH_DASHBOARD.root },
-            { name: 'Publication', href: PATH_DASHBOARD.publication.root },
-            { name: 'New Publication' },
-          ]}
-        />
 
-        <PublicationNewForm />
+        <PublicationNewForm isEdit={isEdit} dataPostUpdate={postUpdate} />
       </Container>
     </Page>
   );
